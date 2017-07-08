05df39905d0a1e2667802be1a154477b5495ba4f
Alex Rossi <rx.rossi@gmail.com>
2017-07-08 16:11:59 -0300

    Added action/login and a test for it, currently nocked (nock library), next step is to write a test against store and make user of dispatch

---
+++ src/actions/login.js
@@ -0,0 +1,21 @@
+import config from '../config';
+import request from 'superagent';
+
+export const loginRoute = config.API_URL+'/auth';
+
+export default (email, password) => {
+
+
+	return request
+		.post(loginRoute)
+		.send({email, password})
+		.then(res => {
+			let { token, user } = res.body;
+			user = { ...user, token };
+			localStorage.setItem('user', user);
+		})
+		.catch(err => console.log(err.message))
+
+
+	// return false;
+}
---
+++ src/actions/login.test.js
@@ -0,0 +1,44 @@
+import { createStore, combineReducers } from 'redux';
+import nock from 'nock';
+import login from './login.js';
+import config from '../config';
+
+const loginReducer = (state = {}, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
+
+const reducer = combineReducers({
+	loginReducer
+})
+
+window.localStorage = {
+	setItem: jest.fn(),
+};
+
+const user = {
+	email: "alex@email.com",
+	password: "123",
+	token: 'atok3n'
+}
+
+const store = createStore(reducer);
+
+describe('login action', () => {
+
+	nock(config.API_URL)
+		.post('/auth', {email: user.email, password: user.password})
+		.reply(200, {user, token: user.token})
+
+	beforeEach( async () => {
+		jest.clearAllMocks();
+		await login(user.email, user.password);
+	})
+
+	it('calls the localStorage.setItem with a token', () => {
+		//user should have user.token
+		expect(localStorage.setItem).toHaveBeenCalledWith('user', user);
+	})
+})
